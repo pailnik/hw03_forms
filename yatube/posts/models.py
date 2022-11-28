45918b9ac160bc03d5572e9ad160706801df68fa
Dmitry Tsybulenko <45.vilka@mail.ru>
2022-11-28 23:55:51 +0300

    GO Добавлен пагинатор отдельной функцией все тесты в норме, почти исправил ревью

--- yatube/posts/models.py
+++ yatube/posts/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.contrib.auth import get_user_model
-from django.db.models import TextField
+# from django.db.models import TextField
 
 User = get_user_model()
 
